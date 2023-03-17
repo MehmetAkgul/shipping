8a317e6c3b94dcfce835ba36ff932201f75f875a
Mehmet Akgul <mehmetakgul.dev@gmail.com>
2023-03-17 16:23:19 +0300

    Messages.java file created.

---
+++ src/main/java/com/backend/shipping/utils/Messages.java
@@ -0,0 +1,42 @@
+package com.backend.shipping.utils;
+
+public class Messages {
+     public final static String BRANCH_NOT_FOUND_MSG_D = "Error: The Branch with %d could not be found";
+    public final static String BRANCH_NOT_FOUND_MSG_S = "Error: The Branch with %s could not be found";
+    public final static String BRANCH_NAME_IS_ALREADY_USE_MSG = "Error: he Branch name is already in use!";
+
+
+     public final static String CUSTOMER_NAME_IS_ALREADY_USE_MSG = "Error: The Customer name is already in use!";
+     public final static String CUSTOMER_NO_AUTHORIZE_MSG = "Error: You do not have permission to update this record!";
+     public final static String CUSTOMER_NOT_FOUND_MSG_D = "Error: The Customer with %d could not be found";
+     public final static String POINT_NOT_FOUND_MSG_D = "Error: The Point with id number %d could not be found";
+     public final static String CUSTOMER_NOT_FOUND_MSG_S = "Error: The Customer with %s could not be found";
+
+
+    public final static String  POINT_NAME_IS_ALREADY_USE_MSG = "Error: Point name is already in use!";
+    public final static String  POINT_ACTIVE_MSG = "Error: If it is active it cannot be deleted!";
+    public final static String  POINT_NOT_FOUND_ID_MSG = "Error: The Point with id number %d could not be found.";
+    public final static String  POINT_NOT_FOUND_ID_MSG_FOR_BRANCH = "Error: The Branch with id number %d could not be found.";
+
+
+    public final static String SERVICE_ENTITY_NAME_IS_ALREADY_USE_MSG = "Error: Unit name is already in use!";
+    public final static String SERVICE_ENTITY_ACTIVE_MSG = "Error: If it is active it cannot be deleted!";
+    public final static String SERVICE_ENTITY_NOT_FOUND_ID_MSG = "Error: Unit %d is not found.";
+
+
+    public final static String UNIT_NAME_IS_ALREADY_USE_MSG = "Error: Unit name is already in use!";
+    public final static String UNIT_ACTIVE_MSG = "Error: If it is active it cannot be deleted!";
+    public final static String UNIT_NOT_FOUND_ID_MSG = "Error: Unit %d is not found.";
+
+    public final static String USER_INVALID_CREDENTIALS = "Invalid credentials";
+    public final static String USER_NOT_FOUND_MSG = "Error: User with id %d not found";
+    public final static String USER_EMAIL_NOT_FOUND_MSG = "Error: Email %s not found";
+    public final static String USER_EMAIL_IS_ALREADY_IN_USE_MSG = "Error: Email is already in use!";
+    public final static String USER_YOU_DONT_HAVE_PERMISSION_TO_UPDATE_USER_INFO = "You dont have permission to update user info!";
+    public final static String USER_YOU_DONT_HAVE_PERMISSION_TO_DELETE = "You dont have permission to delete user";
+    public final static String USER_YOU_DONT_HAVE_PERMISSION_TO_UPDATE_PASSWORD ="You dont have permission to update password!";
+    public final static String USER_PASSWORD_DOES_NOT_MATCH = "password does not match";
+    public final static String USER_ROLE_NOT_FOUND_MSG = "Error: Role is not found.";
+
+
+}
